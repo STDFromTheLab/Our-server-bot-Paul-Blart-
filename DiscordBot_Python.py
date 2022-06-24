58a4cdc54b8d6fe970f028038b7ae9fe1402f377
STDLab_Monsterman1237 <81536757+monsterman1237@users.noreply.github.com>
2022-06-23 22:26:25 -0500

    Updated files
    Re arranged certain things and re coded others.

--- DiscordBot_Python.py
+++ DiscordBot_Python.py
@@ -1,8 +1,5 @@
-import random
-import asyncio
-import aiohttp
+
 import discord
-import json
 from discord.ext import commands
 from discord import Game
 from discord.ext.commands import Bot
@@ -10,22 +7,24 @@ import datetime
 from discord.utils import get
 import time
 
+Contents = open("Token.txt", "r")
+if Contents.mode == 'r':
+    Token = Contents.read()
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
-        print ('Totally not spying on the server ;)'.format(self.user))
+        print('Totally not spying on the server ;)'.format(self.user))
+
     async def on_message(self, message):
-        print('Message from {0.author} in {0.channel}: {0.content}'.format(message))
+        print(
+            'Message from {0.author} in {0.channel}: {0.content}'.format(message))
         if message.author == client.user:
-        
-            print('Message from Bot detected... skipping respond')
+
+            print('Message from Bot detected... skipping response')
             return
-        if message.content.startswith('beaner gang'):           
-            await message.channel.send('beaner gang is ass')
-        
-        
-Token = open("Token.txt", "r")
-if Token.mode == 'r':
-    contents = Token.read()
+
+
 client = MyClient()
-client.run(contents)
+client.run(Token)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Our-server-bot-Paul-Blart-
-The code for our bot so, if you like it you can have it
+# Paul-Blart-Bot-
+
+The code for our bot. so if you like it, you can have it
 Its coded in python using the Discord.py library
-you can use the code by replacing the "your Token" with your discord bot token
+you can use the code by replacing the, "Token" in the token.txt file, with your discord bot token
